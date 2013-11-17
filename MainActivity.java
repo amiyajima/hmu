abc7a6a55c410e6b2d3bef2aef0e75f58ab6ceec
amiyajima <am437@duke.edu>
2013-11-17 12:57:17 -0500

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.hackathonapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+	
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    /*
+     * This method will be called when the user clicks the log in button
+     */
+    public void changeScreen(View myView){
+    	Intent intent = new Intent(this, EventShower.class);
+    	startActivity(intent);
+    }
+    
+        
+}
