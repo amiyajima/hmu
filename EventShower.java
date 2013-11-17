aac68be0bfe755ea7b3265de3f044c0ecb2324d5
amiyajima <am437@duke.edu>
2013-11-17 12:56:38 -0500

    Create EventShower.java

---
+++ EventShower.java
@@ -0,0 +1,59 @@
+package com.example.hackathonapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.support.v4.app.NavUtils;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+
+public class EventShower extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_event_shower);
+		setupActionBar();
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.event_shower, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void changeViewer(View myView){
+    	Intent intent = new Intent(this, EventsList.class);
+    	startActivity(intent);
+    }
+	
+	public void createEventScreen(View myView){
+		Intent intent = new Intent(this, EventCreatorActivity.class);
+		startActivity(intent);
+	}
+	
+}
